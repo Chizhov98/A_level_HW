e6266fa60326bf73c7f1d24e74e458698a6ef44f
Chizhov98 <chizhov1998a@gmail.com>
2021-05-08 15:25:49 +0300

    Homework 4

---
+++ Artem Chizhov/src/ua/chizhov/hw4/Task3.java
@@ -0,0 +1,39 @@
+package ua.chizhov.hw4;
+
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] numArray = initializeIntArray(1000);
+        System.out.println(compositeCount(numArray));
+    }
+
+    public static int[] initializeIntArray(int length) {
+        int[] array = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+
+    public static int compositeCount(int[] array) {
+        int count = 0;
+        for (int element : array) {
+            if (element >= 3 && !checkPrime(element)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean checkPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ Artem Chizhov/src/ua/chizhov/hw4/Task4.java
@@ -0,0 +1,25 @@
+package ua.chizhov.hw4;
+
+import java.util.Random;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] numArray = initializeIntArray(2000);
+        zeros(numArray);
+    }
+
+    public static int[] initializeIntArray(int length) {
+        int[] array = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+
+    public static void zeros(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] % 2 == 0 ? 0 : array[i]);
+        }
+    }
+}
